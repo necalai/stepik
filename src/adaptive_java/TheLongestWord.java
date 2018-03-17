b63f6000becc554f19ea2985975b36ffe8c8e6dd
necalai <nivavilin@gmail.com>
2018-03-17 12:11:28 +0300

    One more

---
+++ src/adaptive_java/TheLongestWord.java
@@ -0,0 +1,13 @@
+package adaptive_java;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class TheLongestWord {
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.stream(new Scanner(System.in).nextLine().split(" "))
+                .max(Comparator.comparingInt(String::length)).get());
+    }
+}
