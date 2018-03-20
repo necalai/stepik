cc17a0a574e615a38f3ae18ddd468dd0b703f312
necalai <nivavilin@gmail.com>
2018-03-20 14:37:32 +0300

    Some changes

--- src/adaptive_java/ChessBoard.java
+++ src/adaptive_java/ChessBoard.java
@@ -12,9 +12,6 @@ public class ChessBoard {
     }
 
     private static boolean canHitEachOther(int x1, int y1, int x2, int y2) {
-        if (x1 == x2 || y1 == y2) {
-            return true;
-        }
-        return Math.abs(x1 - x2) == Math.abs(y1 - y2);
+        return x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2);
     }
 }
